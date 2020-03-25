48d19afb6be2c45e1952f96461cb1141fb4e1fe1
Slushmier <aschless@gwu.edu>
2020-03-25 15:26:25 -0400

    Pulls and updates Atlantic's testing data API

---
+++ testing_data.R
@@ -0,0 +1,12 @@
+library(jsonlite)
+library(readr)
+library(lubridate)
+
+queryurl <- "https://covidtracking.com/api/states/daily"
+
+testing <- jsonlite::read_json(queryurl, simplifyVector = T)
+testing[is.na(testing)] <- 0
+testing$date <- as_date(as.character(testing$date), format = "%Y%m%d", tz = "UTC")
+testing <- arrange(testing, state, date)
+
+write_csv(testing, "Data//testing_atlantic.csv")
