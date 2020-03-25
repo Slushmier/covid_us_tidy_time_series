2d634fe9090a838921ce35eb032b61b224af0f58
Slushmier <aschless@gwu.edu>
2020-03-25 17:46:21 -0400

    Added 'All US' option; moved some data processing to covid_pull.R

--- app.R
+++ app.R
@@ -5,23 +5,39 @@ library(forecast)
 library(scales)
 
 state_output <- function(input_state){
+  if(input_state == "All US") {
+    filtered <- covid_us %>% dplyr::filter(Type != "Recovered")
+  } else {
+    filtered <- covid_ts %>% dplyr::filter(state == input_state)
+    }
   
-  filtered <- covid_ts %>% dplyr::filter(state == input_state)
   disp_date <- dplyr::filter(filtered, Type == 'Active', Number >= 1)
-  disp_date <- disp_date[1, 2]
+  if(input_state == "All US"){
+    disp_date <- disp_date[1, 1]
+  } else{
+      disp_date <- disp_date[1, 2]
+      }
   
-  filtered_gg <- ggplot(data = filtered) +
-    geom_line(aes(x = date, y = Number, color = Type)) + xlab("Date")
-  
-  if (max(filtered$Number) > 10000){
-    filtered_gg <- filtered_gg +
+  filtered_gg <- ggplot(data = filtered) + xlab("Date")  
+    
+  if (max(filtered$Number) > 20000){
+    filtered_gg <- filtered_gg + 
+      geom_smooth(aes(x = date, y = Number, color = Type),
+                  se = F, size = 0.75) +
+      geom_forecast(data = dplyr::filter(filtered, Type == "Active"), 
+                    aes(x = date, y = Number),
+                    color = "red", showgap = F, size = 0.75) +
       scale_y_continuous(trans = 'log10', labels = comma) +
       ylab("Number - Logistic Scale")
+  } else {
+    filtered_gg <- filtered_gg + 
+      geom_line(aes(x = date, y = Number, color = Type), size = 0.75) +
+      geom_forecast(data = dplyr::filter(filtered, Type == "Active"),
+                    aes(x = date, y = Number),
+                    color = "red", showgap = F, size = 0.75)
   }
   
-  filtered_gg + geom_forecast(data = dplyr::filter(filtered, Type == "Active"),
-                              aes(x = date, y = Number), color = "red",
-                              showgap = F) +
+  filtered_gg +
     ggtitle(paste0(input_state, " Covid Cases and Projections")) +
     theme(legend.title.align=0.5) +
     scale_x_date(date_breaks = "1 week",
@@ -31,22 +47,16 @@ state_output <- function(input_state){
 
 covid_ts <- read_csv("https://raw.githubusercontent.com/Slushmier/covid_us_tidy_time_series/master/covid_us_time_series.csv")
 covid_ts$date <- as_date(covid_ts$date, format = "%m/%d/%Y", tz = "UTC")
-covid_ts <- covid_ts %>% mutate(Active = Confirmed - Deaths - Recovered,
-                                New = Active - lag(Active)) %>% 
-  replace_na(list(Active = 0, New = 0)) %>% 
-  dplyr::filter(!is.na(state)) %>% select(-Recovered)
-covid_ts$New <- ifelse(covid_ts$New < 0, 0, covid_ts$New)
-# us_all <- covid_ts %>% group_by(date) %>% 
-#   summarize(Confirmed = sum(Confirmed), Deaths = sum(Deaths),
-#                                  Recovered = sum(Recovered)) %>% 
-#   mutate(Active = Confirmed - Deaths - Recovered, state = "US")
 covid_ts <- covid_ts %>% gather(key = "Type", value = "Number", 
                                 Confirmed, Deaths, New, Active)
-# us_all <- us_all %>% gather(key = "Type", value = "Number",
-#                             Confirmed, Deaths, Recovered, Active)
+
+covid_us <- read_csv("https://raw.githubusercontent.com/Slushmier/covid_us_tidy_time_series/master/covid_us_time_series_aggregate.csv") %>% 
+  gather(key = "Type", value = "Number",
+         Confirmed, Deaths, Recovered, Active, New)
+covid_us$date <- as_date(covid_us$date, format = "%m/%d/%Y", tz = "UTC")
 
 states <- covid_ts %>% dplyr::distinct(state) %>% arrange()
-# covid_ts <- rbind(us_all, covid_ts)
+states <- rbind("All US", states)
 
 ui <- fluidPage(
   titlePanel("Covid-19 Cases by State with 10-day Projections"),
@@ -57,7 +67,7 @@ ui <- fluidPage(
       
       selectInput("stateinput",
               label = "State:",
-              selected = "DC",
+              selected = "All US",
               choices = states),
       p("The red bands are ten day projections for numbers of Covid-19 cases.
         The narrow, dark red bands are 95% confidence intervals, the wider, 
@@ -67,9 +77,6 @@ ui <- fluidPage(
         https://github.com/CSSEGISandData/COVID-19"),
       br(),
       p("Projections are done with the forecast package in R."),
-      br(),
-      p("The 'recovered' cases have been aggregated into US totals, so are not
-        currently accurate at the state level."),
       br(),
       p("Note: I am definitely not an epidemiologist; ask one for more 
         information on Covid-19.")
