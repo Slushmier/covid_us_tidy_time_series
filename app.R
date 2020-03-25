e279840aca7baf746e2d5a43057fcc84a94d984f
Slushmier <aschless@gwu.edu>
2020-03-25 09:59:44 -0400

    Removed 'recovered' cases; added 'New' cases; log transform for graphs with high case numbers. Comments have working code for adding a US aggregate count.

--- app.R
+++ app.R
@@ -9,14 +9,21 @@ state_output <- function(input_state){
   filtered <- covid_ts %>% dplyr::filter(state == input_state)
   disp_date <- dplyr::filter(filtered, Type == 'Active', Number >= 1)
   disp_date <- disp_date[1, 2]
-
+  
   filtered_gg <- ggplot(data = filtered) +
-    geom_line(aes(x = date, y = Number, color = Type))
+    geom_line(aes(x = date, y = Number, color = Type)) + xlab("Date")
+  
+  if (max(filtered$Number) > 10000){
+    filtered_gg <- filtered_gg +
+      scale_y_continuous(trans = 'log10', labels = comma) +
+      ylab("Number - Logistic Scale")
+  }
+  
   filtered_gg + geom_forecast(data = dplyr::filter(filtered, Type == "Active"),
                               aes(x = date, y = Number), color = "red",
                               showgap = F) +
     ggtitle(paste0(input_state, " Covid Cases and Projections")) +
-    theme(legend.title.align=0.5) + xlab("Date") +
+    theme(legend.title.align=0.5) +
     scale_x_date(date_breaks = "1 week",
                  labels = date_format("%d-%b"),
                  limits = (c(disp_date$date - 5, Sys.Date() + 10)))
@@ -24,14 +31,25 @@ state_output <- function(input_state){
 
 covid_ts <- read_csv("https://raw.githubusercontent.com/Slushmier/covid_us_tidy_time_series/master/covid_us_time_series.csv")
 covid_ts$date <- as_date(covid_ts$date, format = "%m/%d/%Y", tz = "UTC")
-covid_ts <- covid_ts %>% mutate(Active = Confirmed - Deaths - Recovered)
+covid_ts <- covid_ts %>% mutate(Active = Confirmed - Deaths - Recovered,
+                                New = Active - lag(Active)) %>% 
+  replace_na(list(Active = 0, New = 0)) %>% 
+  dplyr::filter(!is.na(state)) %>% select(-Recovered)
+covid_ts$New <- ifelse(covid_ts$New < 0, 0, covid_ts$New)
+# us_all <- covid_ts %>% group_by(date) %>% 
+#   summarize(Confirmed = sum(Confirmed), Deaths = sum(Deaths),
+#                                  Recovered = sum(Recovered)) %>% 
+#   mutate(Active = Confirmed - Deaths - Recovered, state = "US")
 covid_ts <- covid_ts %>% gather(key = "Type", value = "Number", 
-                                Confirmed, Deaths, Recovered, Active)
+                                Confirmed, Deaths, New, Active)
+# us_all <- us_all %>% gather(key = "Type", value = "Number",
+#                             Confirmed, Deaths, Recovered, Active)
 
 states <- covid_ts %>% dplyr::distinct(state) %>% arrange()
+# covid_ts <- rbind(us_all, covid_ts)
 
 ui <- fluidPage(
-  titlePanel("Covid-19 cases by state and 10-day projections"),
+  titlePanel("Covid-19 Cases by State with 10-day Projections"),
   
   
   sidebarLayout(
@@ -68,7 +86,5 @@ server <- function(input, output){
     state_output(input$stateinput)
   })
 }
-
-
 
 shinyApp(ui, server)
