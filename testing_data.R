2909e9373cc43699ce41d0c8b0e931f1c13e70e7
Slushmier <aschless@gwu.edu>
2020-04-07 11:20:03 -0400

    Cleans up rows that are not being updated, and carries over some data to fix missing values

--- testing_data.R
+++ testing_data.R
@@ -10,4 +10,11 @@ testing[is.na(testing)] <- 0
 testing$date <- as_date(as.character(testing$date), format = "%Y%m%d", tz = "UTC")
 testing <- arrange(testing, state, date)
 
+testing <- testing %>% group_by(state) %>% 
+  select(-hospitalizedCumulative, -inIcuCumulative, -onVentilatorCumulative,
+         -hash, -hospitalized) %>% 
+  mutate_at(c("positive", "negative", "recovered", "death",
+              "totalTestResults", "posNeg"),
+            ~if_else(. < lag(., default = first(.)), lag(.), .))
+  
 write_csv(testing, "Data//testing_atlantic.csv")
